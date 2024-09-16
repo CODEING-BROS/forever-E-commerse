6948f35fe96c0016a2b3ed16b9db3dd2334eed6e
codewithstella <coderluffy35@gmail.com>
2024-09-16 05:32:07 +0530

    fo

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import Image from 'next/image'
-import { assets } from '@/public/assets/frontend_assets/assets'
-import Link from 'next/link'
+import React from 'react';
+import Image from 'next/image';
+import { assets } from '@/public/assets/frontend_assets/assets';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <div className=' '>
+    <div>
       <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
-
         <div>
-          <Link href="/" ><Image src={assets.logo} alt="Company Logo" className='mb-5 w-32' width={128} height={64} /></Link>
+          <Link href="/" >
+            <Image src={assets.logo} alt="Company Logo" className='mb-5 w-32' width={128} height={64} />
+          </Link>
           <p className='w-full md:w-2/3 text-gray-600'>
             We are committed to providing top-notch services and ensuring customer satisfaction at all times. Contact us anytime for inquiries or support. We are here to help!
           </p>
@@ -17,17 +18,17 @@ const Footer = () => {
 
         <div>
           <p className='text-xl font-medium mb-5 uppercase'>Company</p>
-          <ul className='flex flex-col gap-1 text-gray-600 '>
-            <li>
+          <ul className='flex flex-col gap-1 text-gray-600'>
+            <li key="home">
               <Link href="/" className="hover:text-blue-500 cursor-pointer">Home</Link>
             </li>
-            <li>
+            <li key="about">
               <Link href="/about" className="hover:text-blue-500 cursor-pointer">About Us</Link>
             </li>
-            <li>
+            <li key="delivery">
               <Link href="/delivery" className="hover:text-blue-500 cursor-pointer">Delivery</Link>
             </li>
-            <li>
+            <li key="privacy-policy">
               <Link href="/privacy-policy" className="hover:text-blue-500 cursor-pointer">Privacy Policy</Link>
             </li>
           </ul>
@@ -36,9 +37,9 @@ const Footer = () => {
         <div>
           <p className='text-xl font-medium mb-5 uppercase'>Get in Touch</p>
           <ul className='flex flex-col gap-1 text-gray-600'>
-            <li>8941966505</li>
-            <li>deepakcoder023@gmail.com</li>
-            <li>
+            <li key="phone">8941966505</li>
+            <li key="email">deepakcoder023@gmail.com</li>
+            <li key="instagram">
               <a href='https://www.instagram.com/coding.shaka?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==' 
                  target='_blank' 
                  rel='noopener noreferrer' 
@@ -47,15 +48,16 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-
       </div>
 
       <div>
         <hr />
-        <p className='py-5 text-sm text-center'>Copyright 2024@ codewithstella - All Right Reserved.</p>
+        <p className='py-5 text-sm text-center'>
+          Copyright 2024@ codewithstella - All Right Reserved.
+        </p>
       </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
